2811d4a80e4531876c6de2e3f79f8dbc8bebc549
Negven <35338243+Negven@users.noreply.github.com>
2023-07-29 20:01:31 +0200

    feat: change mobile vers

---
+++ index.js
@@ -0,0 +1,31 @@
+const srcBigScreenPattern = "/img/leaders/leaders";
+const srcSmallScreenPattern = "/img/leaders/mobile/leaders";
+const imgLeadersArray = [...document.querySelector(".leaders__line").children];
+const imgGallery = document.querySelector(".gallery__container");
+const mobileWidth = 800;
+const mobileGalleryWidth = 490;
+
+function changeImgLeadersSrc(windowWidth){
+    imgLeadersArray.forEach((img, index) => {
+        const currentSrc = [
+            windowWidth > mobileWidth ? srcBigScreenPattern : srcSmallScreenPattern,
+            index + 1,
+            windowWidth > mobileWidth ? ".png" : ".svg"].join("");
+        if(!img.src.includes(currentSrc)) img.src = currentSrc;
+    })
+}
+
+function changeGalleryWidth(windowWidth) {
+    if(windowWidth < mobileGalleryWidth){
+        imgGallery.style.marginLeft = (-(mobileGalleryWidth - windowWidth) / 2) + 'px'
+    }
+}
+
+onload = () => {
+    changeImgLeadersSrc(window.screen.width);
+    changeGalleryWidth(window.screen.width)
+}
+onresize = () => {
+    changeGalleryWidth(window.screen.width)
+    changeImgLeadersSrc(window.screen.width);
+}
